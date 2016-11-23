31f9f0e37188a89d5078ff0454f80277fbee4cfd
B-Elgy <brenden.elgarten@googlemail.com>
2016-11-23 17:43:28 -0500

    Finished returnGenerators.py
    Supports Several File Types Now

--- PythonLibraries/returnGenerators.py
+++ PythonLibraries/returnGenerators.py
@@ -1,5 +1,6 @@
 #Import Statements
 import datetime
+import os
 import re
 
 #Variable Definitions
@@ -7,17 +8,9 @@ endl = "\n"
 bendl = endl.encode()
 #Content Type Returns
 contentTypes = {"html":"text/html", "js":"application/javascript", "json":"application/json", "txt":"text/plain", "jpg":"image/jpeg", "jpeg":"image/jpeg", "png":"image/png", "css":"text/css"}
-contentTypeReturnFormats = {"html":"text", "js":"text", "json":"text", "txt", "text", "jpg"}
 #Function Definitions
-    #Helper Functions
-        #Get Content Type Header
-def genContentType(f):
-    name, ext = tuple(re.split("\." , f))
-    contentType = contentTypes[ext]
-    
-    
 #Main Functions
-    #Header Function
+#Header Function
 def genHeaders(f):
     #Local Functions and Variable
     headers = ""
@@ -31,10 +24,23 @@ def genHeaders(f):
         statusHeader = "HTTP/1.1 404 Not Found" + endl
     else: #200 OK Status Header
         statusHeader = "HTTP/1.1 200 OK" + endl
-    encodedStatusHeader = statusHeader.encode()
-    headerAppend(statusHeader,encodedStatusHeader)
+    headerAppend(statusHeader)
     #Date Header
     dateHeader = datetime.datetime.now().strftime("Date: %a, %d %b %Y %H:%M:%S EST"+endl)
-    encodedDateHeader = dateHeader.encode()
-    headerAppend(dateHeader, encodedDateHeader)
-    
+    headerAppend(dateHeader)
+    #Content-Type Header
+    contentTypeHeader = "Content-Type: " + contentTypes[re.split("\.",f)[1]] + endl
+    headerAppend(contentTypeHeader)
+    #Content-Length Header
+    contentLengthHeader = "Content-Length: " + len(open("./html/" + f, "rb").read()) + endl
+    headerAppend(contentLengthHeader)
+    headerAppend(endl)
+    return (headers,headers.encode())
+
+#Return Function
+def genReturn(f):
+    if(not os.path.exists("./html/" + f)): #If Page Doesn't Exist
+        f = "404Error.html"
+    headers, encodedHeaders = genHeaders(f)
+    returnResponse = encodedHeaders + open("./html/" + f, "rb").read()
+    return returnResponse
